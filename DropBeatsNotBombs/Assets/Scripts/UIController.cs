9a79b1a7f9294afbc5a801ed8e94716609bbf2bb
Julius Hartmann <julius.94@hotmail.de>
2019-03-13 11:53:40 +0100

    beginning of uicontroller script

---
+++ DropBeatsNotBombs/Assets/Scripts/UIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    [SerializeField] private float _crowdMoodTotalTimeInSeconds = 500;
+    [SerializeField] private Slider _moodSlider;
+
+    private float _bonusTime { get; set; }
+
+    private float _crowdMood;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _crowdMood = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _crowdMood = (_crowdMoodTotalTimeInSeconds - Time.timeSinceLevelLoad + _bonusTime) / _crowdMoodTotalTimeInSeconds;
+        _moodSlider.value = _crowdMood;
+    }
+}
