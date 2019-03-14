64e651b937f9e1f035b288fec4f69d35927ae6b7
Rattenjunge <les.dams96@gmail.com>
2019-03-14 17:31:59 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHH Drums sind fertig

--- DropBeatsNotBombs/Assets/Scripts/Drums.cs
+++ DropBeatsNotBombs/Assets/Scripts/Drums.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Drums : MonoBehaviour
 {
-    public Image Image_Drums;
-    public Button B_Input;
+    public GameObject Image_Drums;
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Image_Drums.enabled = true;
-            B_Input.enabled = true;
-
+            Image_Drums.SetActive(true);
         }
     }
 }
