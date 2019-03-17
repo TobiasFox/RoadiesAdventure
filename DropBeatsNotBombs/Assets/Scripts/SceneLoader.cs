9c88d37e6be5abe4500dc8b29f2f12d5d2ecb7c1
Tobias Fox <tobiasfox@gmx.net>
2019-03-17 14:24:54 +0100

    use async scene loading for intro scene

--- DropBeatsNotBombs/Assets/Scripts/SceneLoader.cs
+++ DropBeatsNotBombs/Assets/Scripts/SceneLoader.cs
@@ -5,8 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    private bool _isDone = false;
+    private AsyncOperation _asyncOperation;
+
+    private void Start()
+    {
+        StartAsyncSceneLoading("JuliusScene");
+    }
+
+    public void StartAsyncSceneLoading(string sceneName)
+    {
+        StartCoroutine(LoadScene(sceneName));
+    }
+
     public void ChangeScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        _asyncOperation.allowSceneActivation = true;
     }
+
+    private IEnumerator LoadScene(string sceneName)
+    {
+        _asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        _asyncOperation.allowSceneActivation = false;
+
+        while (!_isDone)
+        {
+            if (_asyncOperation.progress < .9f)
+            {
+                yield return null;
+            }
+            _isDone = true;
+        }
+        Debug.Log("Loading complete");
+    }
+
+
 }
